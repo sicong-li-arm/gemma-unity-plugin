73bd18209056b8f1157e0b9485d6cc28bf2902dc
Jetha Chan <jetha@google.com>
2025-03-13 20:05:37 +0100

    add gemma3 entries to gemmamodeltype

--- Runtime/Scripts/GemmaModelType.cs
+++ Runtime/Scripts/GemmaModelType.cs
@@ -10,6 +10,10 @@ namespace GemmaCpp
         Griffin2B,
         GemmaTiny,
         Gemma2_2B,
+        Gemma3_4B,
+        Gemma3_1B,
+        Gemma3_12B,
+        Gemma3_27B,
         PaliGemma224,
         PaliGemma2_3B_224,
         PaliGemma2_10B_224
